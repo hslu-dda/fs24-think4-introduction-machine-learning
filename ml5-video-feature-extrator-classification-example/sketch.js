6576caf5c8672b5e6449d918b12fd566d56c239f
Max Frischknecht <info@maxfrischknecht.ch>
2024-02-21 15:06:33 +0100

    feature extraction example

--- ml5-video-feature-extrator-classification-example/sketch.js
+++ ml5-video-feature-extrator-classification-example/sketch.js
@@ -1,17 +1,40 @@
 // Initialize the Image Classifier method with MobileNet
 let mobilenet;
-let video;
-let prediction = '';
+let classifier;
 
+let video;
+let prediction = "test";
+
+let aButton;
+let bButton;
+let trainButton;
 
 function setup() {
   createCanvas(640, 480);
-  mobilenet = ml5.imageClassifier('MobileNet', modelLoaded);
+
   video = createCapture(VIDEO);
   video.size(640, 480);
   // hide the html video element to only display it in the canvas
   video.hide();
   frameRate(25);
+
+  mobilenet = ml5.featureExtractor("MobileNet", modelLoaded);
+  classifier = mobilenet.classification(video, videoReady);
+
+  aButton = createButton("happy");
+  aButton.mousePressed(function () {
+    classifier.addImage("happy");
+  });
+
+  bButton = createButton("sad");
+  bButton.mousePressed(function () {
+    classifier.addImage("sad");
+  });
+  
+  buttonTrain = createButton("Train the Model");
+  buttonTrain.mousePressed(function () {
+    classifier.train(whileTraining);
+  });
 }
 
 function draw() {
@@ -21,22 +44,35 @@ function draw() {
   fill(255, 255, 0);
   textSize(24);
   text(prediction, 20, 24);
-
-  mobilenet.classify(video, gotPrediction)
-
 }
 
 // When the model is loaded
 function modelLoaded() {
-  console.log('Model Loaded!');
+  console.log("Model Loaded!");
 }
 
-function gotPrediction(error, results){
-    // Display error in the console
-    if (error) {
-      console.error(error);
-    }
+// When the video is ready
+function videoReady() {
+  console.log("Video is ready!!!");
+}
+
+function gotPrediction(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
     // The resultss are in an array ordered by confidence.
     // console.log(results);
-    prediction = results[0].label + " " + results[0].confidence
+    prediction = results[0].label + " " + results[0].confidence;
+    classifier.classify(gotPrediction);
+  }
+}
+
+function whileTraining(loss) {
+  if (loss == null) {
+    console.log('Training Complete');
+    classifier.classify(gotPrediction);
+  } else {
+    console.log(loss);
+  }
 }
